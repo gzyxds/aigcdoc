ad380614b98ac9745d23e0137b1bc8cc8db07c7c
艺创AIGC <348310633@qq.com>
2025-09-03 15:04:17 +0800

    chore: 更新依赖文件的缓存引用路径
    更新了.vitepress/cache/deps目录下多个文件的引用路径，包括vue.js、@vueuse/core.js等，以及对应的_metadata.json文件中的hash值和chunk文件名

--- .vitepress/cache/deps/vitepress___@vueuse_integrations_useFocusTrap.js
+++ .vitepress/cache/deps/vitepress___@vueuse_integrations_useFocusTrap.js
@@ -3,13 +3,13 @@ import {
   toArray,
   tryOnScopeDispose,
   unrefElement
-} from "./chunk-6F5JWMBQ.js";
+} from "./chunk-K7NNOIQG.js";
 import {
   computed,
   shallowRef,
   toValue,
   watch
-} from "./chunk-VJWGEPT5.js";
+} from "./chunk-THPMFJZZ.js";
 
 // node_modules/tabbable/dist/index.esm.js
 var candidateSelectors = ["input:not([inert])", "select:not([inert])", "textarea:not([inert])", "a[href]:not([inert])", "button:not([inert])", "[tabindex]:not(slot):not([inert])", "audio[controls]:not([inert])", "video[controls]:not([inert])", '[contenteditable]:not([contenteditable="false"]):not([inert])', "details>summary:first-of-type:not([inert])", "details:not([inert])"];
